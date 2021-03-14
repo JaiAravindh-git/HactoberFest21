ccc7c513f697660aaf72ae4b22d1015b9c1bc1ca
JaiAravindh-git <62341189+JaiAravindh-git@users.noreply.github.com>
2021-03-14 18:58:23 +0530

    Screen + 4, Routes setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:womenselfhelpgroup/screens/groupselect.dart';
+import 'screens/generalledger.dart';
+import 'screens/loandetails.dart';
+import 'screens/login.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginScreen(),
+        '/loan': (context) => LoanDetails(),
+        '/genled': (context) => GenLed(),
+        '/groupselect': (context) => GroupSelect(),
+      },
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
@@ -21,11 +33,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int currentIndex = 0;
 
+  List listOfPages = [
+    LoanDetails(),
+    GenLed(),
+    Center(child: Icon(Icons.monetization_on)),
+    Center(child: Icon(Icons.monetization_on)),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("APP BAR TITLE"),
+        title: Text("மகளிர் சுய உதவி குழு"),
+        backgroundColor: Colors.green,
       ),
       bottomNavigationBar: BottomNavyBar(
         selectedIndex: currentIndex,
@@ -37,31 +57,37 @@ class _HomePageState extends State<HomePage> {
         items: <BottomNavyBarItem>[
           BottomNavyBarItem(
             icon: Icon(Icons.home),
-            title: Text('Savings'),
-            activeColor: Colors.blueAccent,
+            title: Text(
+              'Savings',
+              style: TextStyle(color: Colors.black),
+            ),
+            activeColor: Colors.greenAccent,
             inactiveColor: Colors.black,
           ),
           BottomNavyBarItem(
             icon: Icon(Icons.home),
-            title: Text('Savings'),
-            activeColor: Colors.blueAccent,
+            title: Text(
+              'Loan',
+              style: TextStyle(color: Colors.black),
+            ),
+            activeColor: Colors.green,
             inactiveColor: Colors.black,
           ),
           BottomNavyBarItem(
             icon: Icon(Icons.home),
-            title: Text('Loan'),
+            title: Text('NIL'),
             activeColor: Colors.blueAccent,
-            inactiveColor: Colors.black,
+            inactiveColor: Colors.greenAccent,
           ),
           BottomNavyBarItem(
             icon: Icon(Icons.home),
-            title: Text('Profile'),
-            activeColor: Colors.blueAccent,
+            title: Text('NIL'),
+            activeColor: Colors.greenAccent,
             inactiveColor: Colors.black,
           )
         ],
       ),
-      body: Text("Women Self Help Group"),
+      body: listOfPages[currentIndex],
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,54 +1,107 @@
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+/*
+ Future<bool> loginUser(String phone, BuildContext context) async {
+    FirebaseAuth _auth = FirebaseAuth.instance;
+
+    _auth.verifyPhoneNumber(
+      phoneNumber: phone,
+      timeout: Duration(seconds: 60),
+      verificationCompleted: (AuthCredential credential) async {
+         result = await _auth.signInWithCredential(credential);
+
+         user = result.user;
+
+        if (user != null) {
+          Navigator.pushNamed(context, '/home');
+        }
+      },
+      verificationFailed: (FirebaseAuthException execption) {
+        print(execption);
+      },
+      codeSent: (String verificationId, [int forceSendingToken]) {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Give the code"),
+                content: Column(
+                  children: <Widget>[
+                    TextField(controller: ,),
+                  ],
+                ),
+              );
+            });
+      },
+      codeAutoRetrievalTimeout: null,
+    );
+  }
+  */
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          "மகளிர் சுய உதவி குழு",
-          style: TextStyle(
-            fontSize: 40.0,
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('images/logo.png'),
+          SizedBox(
+            height: 30.0,
           ),
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        Text(
-          "உள்நுழைய",
-          style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
-        ),
-        SizedBox(
-          height: 40.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0.0),
-          child: TextField(
-            keyboardType: TextInputType.phone,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'தொலைபேசி எண்ணை உள்ளிடவும்',
+          Text(
+            "மகளிர் சுய உதவி குழு",
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.green,
             ),
           ),
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        SizedBox(
-          width: 150.0,
-          height: 50.0,
-          child: ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              "Get OTP",
-              style: TextStyle(fontSize: 20.0),
-            ),
-            style: ElevatedButton.styleFrom(
-                primary: Colors.green, onPrimary: Colors.white),
+          SizedBox(
+            height: 20.0,
           ),
-        ),
-      ],
+          Text(
+            "உள்நுழைய",
+            style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 0.0),
+            child: TextField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'தொலைபேசி எண்ணை உள்ளிடவும்',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          SizedBox(
+            width: 150.0,
+            height: 50.0,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/groupselect');
+              },
+              child: Text(
+                "Get OTP",
+                style: TextStyle(fontSize: 20.0),
+              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.green, onPrimary: Colors.white),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
