5fdeffa13c147caf209966490aa5595a9fbc5460
JaiAravindh-git <62341189+JaiAravindh-git@users.noreply.github.com>
2021-03-23 10:46:33 +0530

    UI Work going on

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:womenselfhelpgroup/screens/WelcomePage.dart';
 import 'package:womenselfhelpgroup/screens/adddata.dart';
 import 'package:womenselfhelpgroup/screens/fetchData.dart';
 import 'package:womenselfhelpgroup/screens/groupselect.dart';
+import 'package:womenselfhelpgroup/screens/loginpage.dart';
+import 'package:womenselfhelpgroup/screens/signup.dart';
 import 'screens/generalledger.dart';
 import 'screens/loandetails.dart';
-import 'screens/login.dart';
+//import 'screens/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,18 +18,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/login',
+      initialRoute: '/welcome',
       routes: {
+        '/welcome' : (context) => WelcomePage(),
         '/home': (context) => HomePage(),
-        '/login': (context) => LoginScreen(),
+        '/login': (context) => LoginPage(),
         '/loan': (context) => LoanDetails(),
         '/genled': (context) => GenLed(),
         '/groupselect': (context) => GroupSelect(),
         '/fetchdata':(context) => FetchData(),
         '/addData': (context) => AddData(),
+        '/signup' : (context) => SignUp(),
       },
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
@@ -60,6 +66,12 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("மகளிர் சுய உதவி குழு"),
         backgroundColor: Colors.green,
+        actions: [
+          ElevatedButton(onPressed: (){
+              FirebaseAuth.instance.signOut();
+              Navigator.pushNamed(context, '/login');
+          }, child: Icon(Icons.logout))
+        ],
       ),
       bottomNavigationBar: BottomNavyBar(
         selectedIndex: currentIndex,
